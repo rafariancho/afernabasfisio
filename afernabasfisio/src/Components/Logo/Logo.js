6b4bb07ddd3567675a10ec3f9bed407c46c33258
rafariancho <rafariancho@gmail.com>
2018-03-21 09:04:05 +0000

    Logo

--- afernabasfisio/src/Components/Logo/Logo.js
+++ afernabasfisio/src/Components/Logo/Logo.js
@@ -5,7 +5,8 @@ class Logo extends Component {
     render() {
       return (
         <div className="ImgContainer">
-          <img src={require("../../imgs/Logo.png")} width="35%" className="logo"/>
+          <div className="layer1"><img src={require("../../imgs/Logo.png")} width="35%" className="logo"/> </div>
+          <img src={require("../../imgs/masaje.jpg")} width="100%" className="imgBackground"/>
         </div>
       );
     }
